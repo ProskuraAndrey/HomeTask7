1fc480ad270ca48c9350647b5ad6057f35746047
Alex <proskuraaleksandr93@gmail.com>
2018-04-22 12:54:59 +0300

    custom generic list

---
+++ GenericList/GenericList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[4] { 1, 2, 3, 4 };
+            int[] b = new int[4];
+
+            int index = 0;
+
+            a.CopyTo(b, index);
+            foreach (int elements in a)
+                Console.WriteLine(elements);
+            Console.ReadLine();
+        }
+    }
+}
